e3a739807f6ca37e168a70d752904f999706fa83
senseiseun <seunsuberu01@gmail.com>
2023-02-02 20:41:08 -0600

    remove_duplicates pure function

---
+++ README.md
@@ -0,0 +1,4 @@
+# COVID Information CS5331 Data Mining Project 1
+
+## [Functional Programming](https://medium.com/@kumbhar.pradnya/functional-programming-principles-6f59bc6764ff) will be strongly utilized throughout this project.
+
--- src/util.R
+++ src/util.R
@@ -1,3 +1,5 @@
+#data cleansing
+
 reduce_outliers <- function(dt, col) {
   quartiles <- quantile(dt[,col], probs = c(0,0.25,0.5,0.75,1));
   print(quartiles)
@@ -11,11 +13,27 @@ reduce_outliers <- function(dt, col) {
   return(new_dt)
 }
 
+remove_duplicates <- function(dt) {
+  library("r2r")
+  set <- hashset()
+  new_df <- dt[0,]
+  for (i in 1:nrow(dt)) {
+    str_rep <- toString(dt[i,])
+    print(str_rep)
+    if (!has_key(set, str_rep)) {
+      insert(set, str_rep)
+      new_df <- rbind(new_df, dt[i,])
+    }
+  }
+  return(new_df)
+}
+
 test_reduce_outliers <- function() {
   df <- data.frame (name  = c("Seun Suberu", "Jasmine Patrick", "Julian Brown"),
                     birth_year = c(2001, 2009, 2001),
                     birth_month = c("April", "November", "January"),
                     birth_day = c(6, 27, 2))
+  df <- remove_duplicates(df)
   df <- reduce_outliers(df, "birth_year")
   View(df)
 }
